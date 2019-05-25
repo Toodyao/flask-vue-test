2332dce663dfdff9ac0fc311cec4b4958bba5e5d
Toodyao-XPS-13 <toodyao@gmail.com>
2019-05-25 21:35:14 +0800

    Add RESTful API support using axios.

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+// 引入Bootstrap样式
+import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
 
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
   render: h => h(App)
 }).$mount('#app')
