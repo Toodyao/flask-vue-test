42ee5f9c994485b16e8f6626d9ee4bcfa2bbd8e0
Toodyao-XPS-13 <toodyao@gmail.com>
2019-05-26 20:18:23 +0800

    Add db, users and user token.

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
+
+# Flask-SQLAlchemy 插件
+db = SQLAlchemy()
+# Flask-Migrate 插件
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -9,6 +16,10 @@ def create_app(config_class=Config):
 
     # 启动CORS
     CORS(app)
+    # 初始化Flask-SQLAlchemy
+    db.init_app(app)
+    # 初始化Flask-Migrate
+    migrate.init_app(app, db)
 
     # 注册blueprint
     from app.api import bp as api_bp
@@ -16,3 +27,4 @@ def create_app(config_class=Config):
 
     return app
 
+from app import models
